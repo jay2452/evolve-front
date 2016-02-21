d05fc52b47676ec1ba00018c21cd178043b1716a
jay2452 <jay2452@live.com>
2016-02-21 12:48:21 +0530

    school added

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,5 +3,4 @@ class School < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
 end
